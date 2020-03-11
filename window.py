a8752c6ee1be19e0e14e50dac1b655c130966a9e
Kaeldehta-Laptop <floxx33@gmail.com>
2020-03-11 15:22:42 +0100

    Update window.py
    Added function to get windows for list of squares

--- window.py
+++ window.py
@@ -76,4 +76,34 @@ def get_windows(contents, window_size = 7):
         window = contents_sliced[y - half : y + half + 1, x - half : x + half + 1, : ]
         windows.append(window)
 
-    return np.array(windows)
+    return owned_squares, np.array(windows)
+
+def get_windows_for_squares(contents, owned_squares, window_size = 7):
+    map_size_x = len(contents[0])
+    map_size_y = len(contents)
+
+    contents_sliced = np.zeros((map_size_y, map_size_x, 6))
+
+    for row in range(map_size_y):
+        for col in range(map_size_x):
+
+            square = contents[row][col]
+
+            contents_sliced[row, col, 0] = square.strength if square.owner == 1 else 0
+            contents_sliced[row, col, 1] = square.strength if square.owner == 2 else 0
+            contents_sliced[row, col, 2] = square.strength if square.owner == 0 else 0
+
+            # production of player 1, player 2 and neutral
+            contents_sliced[row, col, 3] = square.production if square.owner == 1 else 0
+            contents_sliced[row, col, 4] = square.production if square.owner == 2 else 0
+            contents_sliced[row, col, 5] = square.production if square.owner == 0 else 0
+
+    windows = []
+    for x, y in owned_squares:
+
+        half = int(window_size / 2)
+
+        window = contents_sliced[y - half : y + half + 1, x - half : x + half + 1, : ]
+        windows.append(window)
+
+    return owned_squares, np.array(windows)
